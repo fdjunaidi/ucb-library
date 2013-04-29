663bef65ea493ca26a936139188cb77571ac6a1e
fdjunaidi <fdjunaidi@berkeley.edu>
2013-04-29 01:23:20 -0700

    Initial Commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UcbLibrary::Application.config.secret_token = '17d88d79e86eb06310c089ecf0244c1a4334729ede63b19286ef8395cffeb87ad9e2c6fcc1b119863c633228eee014395c7671c8bfa6f1be1be3aaa2fcf4e8b7'
